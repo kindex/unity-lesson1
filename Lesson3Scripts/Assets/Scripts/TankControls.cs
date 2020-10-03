8ace10c7058a9a89cb1d01628f9066183ac9bfa4
kindex <kindex@kindex.lv>
2020-10-03 22:57:55 +0300

    Lesson 3: Scripts

---
+++ Lesson3Scripts/Assets/Scripts/TankControls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class TankControls : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<WheelJoint2D>().useMotor = true;
+            var jointMotor2D = GetComponent<WheelJoint2D>().motor;
+            jointMotor2D.motorSpeed = 1000;
+            GetComponent<WheelJoint2D>().motor = jointMotor2D;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<WheelJoint2D>().useMotor = true;
+            var jointMotor2D = GetComponent<WheelJoint2D>().motor;
+            jointMotor2D.motorSpeed = -1000;
+            GetComponent<WheelJoint2D>().motor = jointMotor2D;
+        }
+        else
+        {
+            GetComponent<WheelJoint2D>().useMotor = false;
+        }
+    }
+}
---
+++ Lesson3Scripts/README.md
@@ -0,0 +1,8 @@
+# Unity Lesson 3 : Scripts
+Unity toturial for Untiy 2019.4
+
+Topics:
+C# Components
+Script fot Tank motor
+
+(C) Andrey Pelipenko KindeX
